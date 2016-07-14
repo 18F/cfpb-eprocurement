577c1522cc2de84b1b1ac8c23890aec0d23c0691
Shawn Allen <shawn.allen@gsa.gov>
2016-07-13 20:38:08 -0700

    add querystring

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -10,5 +10,7 @@ global.moment = require('moment');
 global.d3 = require('d3');
 global.colorbrewer = require('colorbrewer');
 
+global.querystring = require('querystring');
+
 require('cf-expandables');
 require('cf-tables');
--- static/js/main.js
+++ static/js/main.js
@@ -35731,6 +35731,185 @@ return jQuery;
 
 }));
 },{}],12:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+// If obj.hasOwnProperty has been overridden, then calling
+// obj.hasOwnProperty(prop) will break.
+// See: https://github.com/joyent/node/issues/1707
+function hasOwnProperty(obj, prop) {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+module.exports = function(qs, sep, eq, options) {
+  sep = sep || '&';
+  eq = eq || '=';
+  var obj = {};
+
+  if (typeof qs !== 'string' || qs.length === 0) {
+    return obj;
+  }
+
+  var regexp = /\+/g;
+  qs = qs.split(sep);
+
+  var maxKeys = 1000;
+  if (options && typeof options.maxKeys === 'number') {
+    maxKeys = options.maxKeys;
+  }
+
+  var len = qs.length;
+  // maxKeys <= 0 means that we should not limit keys count
+  if (maxKeys > 0 && len > maxKeys) {
+    len = maxKeys;
+  }
+
+  for (var i = 0; i < len; ++i) {
+    var x = qs[i].replace(regexp, '%20'),
+        idx = x.indexOf(eq),
+        kstr, vstr, k, v;
+
+    if (idx >= 0) {
+      kstr = x.substr(0, idx);
+      vstr = x.substr(idx + 1);
+    } else {
+      kstr = x;
+      vstr = '';
+    }
+
+    k = decodeURIComponent(kstr);
+    v = decodeURIComponent(vstr);
+
+    if (!hasOwnProperty(obj, k)) {
+      obj[k] = v;
+    } else if (isArray(obj[k])) {
+      obj[k].push(v);
+    } else {
+      obj[k] = [obj[k], v];
+    }
+  }
+
+  return obj;
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+},{}],13:[function(require,module,exports){
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+'use strict';
+
+var stringifyPrimitive = function(v) {
+  switch (typeof v) {
+    case 'string':
+      return v;
+
+    case 'boolean':
+      return v ? 'true' : 'false';
+
+    case 'number':
+      return isFinite(v) ? v : '';
+
+    default:
+      return '';
+  }
+};
+
+module.exports = function(obj, sep, eq, name) {
+  sep = sep || '&';
+  eq = eq || '=';
+  if (obj === null) {
+    obj = undefined;
+  }
+
+  if (typeof obj === 'object') {
+    return map(objectKeys(obj), function(k) {
+      var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+      if (isArray(obj[k])) {
+        return map(obj[k], function(v) {
+          return ks + encodeURIComponent(stringifyPrimitive(v));
+        }).join(sep);
+      } else {
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+      }
+    }).join(sep);
+
+  }
+
+  if (!name) return '';
+  return encodeURIComponent(stringifyPrimitive(name)) + eq +
+         encodeURIComponent(stringifyPrimitive(obj));
+};
+
+var isArray = Array.isArray || function (xs) {
+  return Object.prototype.toString.call(xs) === '[object Array]';
+};
+
+function map (xs, f) {
+  if (xs.map) return xs.map(f);
+  var res = [];
+  for (var i = 0; i < xs.length; i++) {
+    res.push(f(xs[i], i));
+  }
+  return res;
+}
+
+var objectKeys = Object.keys || function (obj) {
+  var res = [];
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) res.push(key);
+  }
+  return res;
+};
+
+},{}],14:[function(require,module,exports){
+'use strict';
+
+exports.decode = exports.parse = require('./decode');
+exports.encode = exports.stringify = require('./encode');
+
+},{"./decode":12,"./encode":13}],15:[function(require,module,exports){
 require('document-register-element');
 
 var prefix = 'cfpb-';
@@ -35750,7 +35929,7 @@ module.exports = {
   PopupMenu: PopupMenu
 };
 
-},{"./popup-menu":13,"./sortable-table":14,"document-register-element":8}],13:[function(require,module,exports){
+},{"./popup-menu":16,"./sortable-table":17,"document-register-element":8}],16:[function(require,module,exports){
 var EVENTS = '__events__';
 
 var EXPANDED = 'aria-expanded';
@@ -35939,7 +36118,7 @@ var PopupMenu = {
 
 module.exports = PopupMenu;
 
-},{"../lib/events":16,"../lib/get-ancestor":17}],14:[function(require,module,exports){
+},{"../lib/events":19,"../lib/get-ancestor":20}],17:[function(require,module,exports){
 var DATA_KEY = '__data__';
 var SORT_KEY = '__sort__';
 var FILTER_KEY = '__filter__';
@@ -36186,7 +36365,7 @@ var SortableTable = {
 
 module.exports = SortableTable;
 
-},{"../lib/compare":15,"../lib/events":16,"../lib/get-ancestor":17,"../lib/predicate":18}],15:[function(require,module,exports){
+},{"../lib/compare":18,"../lib/events":19,"../lib/get-ancestor":20,"../lib/predicate":21}],18:[function(require,module,exports){
 var ASCENDING = 'ascending';
 var DESCENDING = 'descending';
 var NONE = 'none';
@@ -36210,7 +36389,7 @@ compare[DESCENDING] = function desc(a, b) {
 
 module.exports = compare;
 
-},{}],16:[function(require,module,exports){
+},{}],19:[function(require,module,exports){
 var getAncestor = require('./get-ancestor');
 
 var functor = function(d) {
@@ -36244,7 +36423,7 @@ module.exports.delegate = function delegate(selectors) {
   };
 };
 
-},{"./get-ancestor":17}],17:[function(require,module,exports){
+},{"./get-ancestor":20}],20:[function(require,module,exports){
 module.exports = function getAncestor(element, selector) {
   while (element = element.parentNode) {
     if (element !== document && element.matches(selector)) {
@@ -36253,7 +36432,7 @@ module.exports = function getAncestor(element, selector) {
   }
 };
 
-},{}],18:[function(require,module,exports){
+},{}],21:[function(require,module,exports){
 module.exports = function predicate(filters) {
   var functions = Object.keys(filters).map(function(key) {
     var value = filters[key];
@@ -36277,7 +36456,7 @@ module.exports = function predicate(filters) {
   };
 };
 
-},{}],19:[function(require,module,exports){
+},{}],22:[function(require,module,exports){
 (function (global){
 'use strict';
 
@@ -36291,8 +36470,10 @@ global.moment = require('moment');
 global.d3 = require('d3');
 global.colorbrewer = require('colorbrewer');
 
+global.querystring = require('querystring');
+
 require('cf-expandables');
 require('cf-tables');
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./components":12,"cf-expandables":2,"cf-tables":4,"colorbrewer":6,"d3":7,"jquery":10,"moment":11}]},{},[19]);
+},{"./components":15,"cf-expandables":2,"cf-tables":4,"colorbrewer":6,"d3":7,"jquery":10,"moment":11,"querystring":14}]},{},[22]);
