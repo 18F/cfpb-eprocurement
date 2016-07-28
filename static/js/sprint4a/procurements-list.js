6983a3ceb45c306981fcf97983b5e058b4db3ac7
Shawn Allen <shawn.allen@gsa.gov>
2016-07-28 15:43:09 -0700

    re-include querystring

---
+++ static/js/sprint4a/procurements-list.js
@@ -0,0 +1,40 @@
+(function(exports) {
+
+  var procs = d3.selectAll('section.procurement')
+    .datum(function() {
+      var json = this.getAttribute('data-json');
+      var data = JSON.parse(json || '{}');
+      data.favorite = this.querySelector('input[type=checkbox]').checked;
+      return data;
+    })
+    .attr('hidden', null);
+
+  var query = querystring.parse(location.search.substr(1));
+  if (query.sort) {
+    var sortField = query.sort;
+
+    // update the sort input
+    d3.select('[name=sort]')
+      .property('value', sortField);
+
+    // the default sort is by requested_award_date
+    var defaultField = 'requested_award_date';
+    var defaultOrder = d3[query.order] || d3.ascending;
+    var defaultSort = function(a, b) {
+      return defaultOrder(a[defaultField], b[defaultField]);
+    };
+
+    procs.sort((sortField === defaultField)
+      ? defaultSort
+      : function(a, b) {
+          return defaultOrder(a[sortField], b[sortField])
+              || defaultSort(a, b);
+        });
+  }
+
+  d3.selectAll('form.autosubmit')
+    .on('change', function() {
+      this.submit();
+    });
+
+})(this);
